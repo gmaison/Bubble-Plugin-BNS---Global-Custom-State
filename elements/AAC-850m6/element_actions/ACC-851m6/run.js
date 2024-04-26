2f869c761f0fed2168198114c2cd28bbd08fec6b
Guillaume Maison <34339691+gmaison@users.noreply.github.com>
2024-04-26 15:18:36 +0200

    Automatic commit from Bubble

--- elements/AAC-850m6/element_actions/ACC-851m6/run.js
+++ elements/AAC-850m6/element_actions/ACC-851m6/run.js
@@ -3,11 +3,13 @@ function(instance, properties, context) {
 
   //Load any data 
 
+    let values= null;
+    
     if (window.isGcsManagerSet) {
         if (properties.list_value) {
-            let values = properties.list_value.get(0, properties.list_value.length())
-            window.gcsSetListValue(instance, instance.data.gcsDataType, instance.data.gcsName, values, true);
-        }
+            values = properties.list_value.get(0, properties.list_value.length())
+        };
+        window.gcsSetListValue(instance, instance.data.gcsDataType, instance.data.gcsName, values, true);
     }
 
   //Do the operation
